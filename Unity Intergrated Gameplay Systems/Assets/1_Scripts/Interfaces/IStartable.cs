89ead441e06ae306aaceb0f493dd125dbe602ebb
watenk <ivar.nuij@gmail.com>
2023-10-03 10:44:24 +0200

    Added gameManager interfaces

--- Unity Intergrated Gameplay Systems/Assets/1_Scripts/Interfaces/IStartable.cs
+++ Unity Intergrated Gameplay Systems/Assets/1_Scripts/Interfaces/IStartable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IUpdateable
+public interface IStartable 
 {
-    public void OnUpdate();
+    void OnStart();
 }
